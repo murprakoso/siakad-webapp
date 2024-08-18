f52f2d751975854a1f60211fb29d076f7107ac5a
Pecinta Kaffein <prakoso.clan04@gmail.com>
2024-08-18 20:59:49 +0900

    templating

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('home');
+});
+Route::get('/login', function () {
+    return view('auth.login');
 });
