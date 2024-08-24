d20a253549aa963505dbe05343ae860a731d4b0a
Pecinta Kaffein <prakoso.clan04@gmail.com>
2024-08-25 00:39:34 +0900

    modify landing

--- resources/views/landing.blade copy.php
+++ resources/views/landing.blade copy.php
@@ -1,19 +1,13 @@
 <!DOCTYPE html>
-<html lang="id">
+<html lang="en">
 
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="Sistem Informasi Akademik Sekolah" />
-    <meta name="author" content="Tim SIAKAD" />
-    <title>SIAKAD - Sistem Informasi Akademik Sekolah</title>
-    <!-- Favicon -->
-    @if (isset($settings->site_favicon) && $settings->site_favicon)
-        <link rel="icon" href="{{ asset($settings->site_favicon_url) }}" type="image/x-icon">
-    @else
-        <link rel="icon" href="{{ asset('landing/assets/favicon.ico') }}" type="image/x-icon">
-        <!-- Default favicon -->
-    @endif
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>One Page Wonder - Start Bootstrap Template</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts-->
@@ -29,14 +23,30 @@
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
         <div class="container px-5">
-            <a class="navbar-brand" href="#page-top">SIAKAD Sekolah</a>
+            <a class="navbar-brand" href="#page-top">Sikad</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
                 aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
                     class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
+                {{-- <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Beranda</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Login
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="{{ url('siswa/login') }}">Siswa</a></li>
+                            <li><a class="dropdown-item" href="{{ url('guru/login') }}">Guru</a></li>
+                            <li><a class="dropdown-item" href="{{ url('operator/login') }}">Operator</a></li>
+                        </ul>
+                    </li>
+                </ul> --}}
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="{{ url('/') }}">Beranda</a></li>
+
                     @guest
+                        <!-- Jika user belum login, tampilkan opsi login -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
@@ -49,10 +59,11 @@
                             </ul>
                         </li>
                     @else
+                        <!-- Jika user sudah login, tampilkan nama user dan opsi logout -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">
-                                {{ Auth::user()->nama_lengkap }}
+                                {{ Auth::user()->nama_lengkap }} <!-- Menampilkan nama user yang sedang login -->
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="{{ route('dashboard') }}">Dashboard</a></li>
@@ -67,17 +78,17 @@
                         </li>
                     @endguest
                 </ul>
+
             </div>
         </div>
     </nav>
-
     <!-- Header-->
     <header class="masthead text-center text-white">
         <div class="masthead-content">
             <div class="container px-5">
-                <h1 class="masthead-heading mb-0">Selamat Datang di SIAKAD</h1>
-                <h2 class="masthead-subheading mb-0">Sistem Informasi Akademik Sekolah</h2>
-                <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#tentang">Pelajari Lebih Lanjut</a>
+                <h1 class="masthead-heading mb-0">One Page Wonder</h1>
+                <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
+                <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Learn More</a>
             </div>
         </div>
         <div class="bg-circle-1 bg-circle"></div>
@@ -85,73 +96,72 @@
         <div class="bg-circle-3 bg-circle"></div>
         <div class="bg-circle-4 bg-circle"></div>
     </header>
-
     <!-- Content section 1-->
-    <section id="tentang">
+    <section id="scroll">
         <div class="container px-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6 order-lg-2">
                     <div class="p-5"><img class="img-fluid rounded-circle"
-                            src="{{ asset('landing/assets/img/sekolah.png') }}" alt="Sekolah" /></div>
+                            src="{{ asset('landing/assets/img/01.jpg') }}" alt="..." />
+                    </div>
                 </div>
                 <div class="col-lg-6 order-lg-1">
                     <div class="p-5">
-                        <h2 class="display-4">Mengelola Akademik dengan Mudah</h2>
-                        <p>SIAKAD adalah solusi untuk mengelola administrasi akademik sekolah dengan cepat dan efisien.
-                            Dari pendaftaran siswa hingga penilaian akhir, semua dapat dilakukan dalam satu sistem.</p>
+                        <h2 class="display-4">For those about to rock...</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam
+                            sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione
+                            voluptatum molestiae adipisci, beatae obcaecati.</p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-
     <!-- Content section 2-->
     <section>
         <div class="container px-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6">
-                    <div class="p-5"><img class="img-fluid rounded-circle"
-                            src="{{ asset('landing/assets/img/pendidikan.png') }}" alt="Pendidikan" /></div>
+                    <div class="p-5"><img class="img-fluid rounded-circle" src="landing/assets/img/02.jpg"
+                            alt="..." />
+                    </div>
                 </div>
                 <div class="col-lg-6">
                     <div class="p-5">
-                        <h2 class="display-4">Efisiensi dalam Pengelolaan Data</h2>
-                        <p>Sistem kami membantu sekolah mengelola data siswa, guru, dan administrasi lainnya dengan
-                            lebih efisien. Pengelolaan data yang terstruktur memudahkan sekolah untuk mengambil
-                            keputusan yang tepat.</p>
+                        <h2 class="display-4">We salute you!</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam
+                            sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione
+                            voluptatum molestiae adipisci, beatae obcaecati.</p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-
     <!-- Content section 3-->
     <section>
         <div class="container px-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6 order-lg-2">
-                    <div class="p-5"><img class="img-fluid rounded-circle"
-                            src="{{ asset('landing/assets/img/kelas.png') }}" alt="Kelas" /></div>
+                    <div class="p-5"><img class="img-fluid rounded-circle" src="landing/assets/img/03.jpg"
+                            alt="..." />
+                    </div>
                 </div>
                 <div class="col-lg-6 order-lg-1">
                     <div class="p-5">
-                        <h2 class="display-4">Sistem Penilaian yang Terintegrasi</h2>
-                        <p>SIAKAD menyediakan fitur penilaian terintegrasi yang memudahkan guru untuk memberikan nilai
-                            dan laporan secara real-time. Siswa dan orang tua dapat dengan mudah mengakses hasil
-                            akademik melalui portal ini.</p>
+                        <h2 class="display-4">Let there be rock!</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam
+                            sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione
+                            voluptatum molestiae adipisci, beatae obcaecati.</p>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-
     <!-- Footer-->
     <footer class="py-5 bg-black">
         <div class="container px-5">
-            <p class="m-0 text-center text-white small">&copy; SIAKAD 2024. Semua Hak Dilindungi.</p>
+            <p class="m-0 text-center text-white small">&copy; Siakad 2024</p>
         </div>
     </footer>
-
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
